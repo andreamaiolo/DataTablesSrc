5bd4523fb4c42b8a4324596e846ea250c51f71d5
Andrea Maiolo <andrea.maiolo@hotmail.it>
2018-10-07 13:28:53 +0200

    Edit pagination

--- js/integration/dataTables.bulma.js
+++ js/integration/dataTables.bulma.js
@@ -43,18 +43,129 @@
 
     /* Set the defaults for DataTables initialisation */
     $.extend(true, DataTable.defaults, {
-        dom: "<'columns'<'column is-6'l><'column is-6'f>>" +
-            "<'columns'<'column is-12'tr>>" +
-            "<'columns'<'column is-5'i><'column is-7'p>>",
+        dom: "<'columns'<'column is-6'l><'column is-6'f>><'columns'<'column is-12'tr>><'columns'<'column is-5'i><'column is-7'p>>",
         renderer: 'bulma'
     });
 
     /* Default class modification */
     $.extend(DataTable.ext.classes, {
-        sLength: 'dataTables_length field',
         sFilter: 'dataTables_filter field',
-        sFilterInput: 'input is-small'
+        sFilterInput: 'input is-small',
+        sLength: 'dataTables_length field',
+        sPageButton: 'pagination-link',
+        sPageButtonActive: 'is-current',
+        sPageButtonNext: 'pagination-next',
+        sPageButtonPrevious: 'pagination-previous',
+        sPageEllipsis: 'pagination-ellipsis',
+        sPaging: 'pagination dataTables_paginate paging_',
+        sWrapper: 'dataTables_wrapper dt-bulma'
     });
+
+    /* Bulma paging button renderer */
+    DataTable.ext.renderer.pageButton.bulma = function (settings, host, idx, buttons, page, pages) {
+        var api = new DataTable.Api(settings);
+        var classes = settings.oClasses;
+        var lang = settings.oLanguage.oPaginate;
+        var aria = settings.oLanguage.oAria.paginate || {};
+        var btnDisplay, btnClass, counter = 0;
+
+        var attach = function (container, buttons) {
+            var i, ien, node, button, isDisabled = false;
+            var clickHandler = function (e) {
+                e.preventDefault();
+
+                if (!$(e.currentTarget).is('disabled') && api.page() != e.data.action) {
+                    api.page(e.data.action).draw('page');
+                }
+            };
+
+            for (i = 0, ien = buttons.length; i < ien; i++) {
+                button = buttons[i];
+
+                if ($.isArray(button)) {
+                    attach(container, button);
+                } else {
+                    btnDisplay = null;
+                    btnClass = '';
+
+                    switch (button) {
+                        case 'ellipsis':
+                            $('<li>')
+                                .append('<span class="' + classes.sPageEllipsis + '">&hellip;</span>')
+                                .appendTo(container);
+                            break;
+
+                        case 'first':
+                            btnDisplay = lang.sFirst;
+                            isDisabled = page > 0 ? false : true;
+                            break;
+
+                        case 'previous':
+                            btnDisplay = lang.sPrevious;
+                            btnClass = classes.sPageButtonPrevious;
+                            isDisabled = page > 0 ? false : true;
+                            break;
+
+                        case 'next':
+                            btnDisplay = lang.sNext;
+                            btnClass = classes.sPageButtonNext;
+                            isDisabled = page < pages - 1 ? false : true;
+                            break;
+
+                        case 'last':
+                            btnDisplay = lang.sLast;
+                            isDisabled = page < pages - 1 ? false : true;
+                            break;
+
+                        default:
+                            btnDisplay = button + 1;
+                            btnClass = page === button ? ' ' + classes.sPageButtonActive : '';
+                            break;
+                    }
+
+                    if (btnDisplay !== null) {
+                        node = $('<li>')
+                            .append($('<a>', {
+                                'class': button !== 'previous' && button !== 'next' ? classes.sPageButton + btnClass : btnClass,
+                                'aria-controls': settings.sTableId,
+                                'aria-label': aria[button],
+                                'data-dt-idx': counter,
+                                'tabindex': settings.iTabIndex,
+                                'id': idx === 0 && typeof button === 'string' ? settings.sTableId + '_' + button : null,
+                                'disabled': isDisabled,
+                            })
+                            .html(btnDisplay))
+                            .appendTo(container);
+
+                        settings.oApi._fnBindAction(node.children('a'), {
+                            action: button
+                        }, clickHandler);
+
+                        counter++;
+                    }
+                }
+            }
+        };
+
+        // IE9 throws an 'unknown error' if document.activeElement is used
+        // inside an iframe or frame. Try / catch the error. Not good for
+        // accessibility, but neither are frames.
+        var activeEl;
+
+        try {
+            // Because this approach is destroying and recreating the paging
+            // elements, focus is lost on the select button which is bad for
+            // accessibility. So we want to restore focus once the draw has
+            // completed
+            activeEl = $(host).find(document.activeElement).data('dt-idx');
+        } catch (e) {}
+
+        attach($(host).empty().html('<ul class="pagination-list"/>').children('ul'), buttons);
+
+        if (activeEl !== undefined) {
+            $(host).find('[data-dt-idx=' + activeEl + ']').focus();
+        }
+    }
 
     return DataTable;
 }));
